129086b50e86cfd35ba3b022738505844894e7e3
Myles Borins <mborins@us.ibm.com>
2015-11-29 17:50:28 -0800

    Moar Stairway

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 var io = require('socket.io-client/socket.io.js');
 
 var freq = require('./audio');
+var stairway = require('./stairway');
+
+var index = 0;
 
 var socket = io();
 
@@ -10,8 +13,25 @@ function update (value) {
   socket.emit('update', value);
 }
 
+function next() {
+  if (index > stairway.length - 1) {
+    index = 0;
+  }
+  update(stairway[index]);
+  index++;
+}
+
+function play() {
+  next();
+  if (index !== stairway.length - 1) {
+    window.setTimeout(play, 500);
+  }
+}
+
 if (master) {
   global.update = update;
+  global.next = next;
+  global.play = play;
 }
 
 socket.on('note', freq.update);
---
+++ src/stairway.js
@@ -0,0 +1,37 @@
+var _ = require('lodash')
+var MIDIUtils = require('midiutils');
+
+var stairway = [
+  'A-2',
+  'C-3',
+  'E-3',
+  'A-3',
+  'B-3',
+  'E-3',
+  'C-3',
+  'B-3',
+  'C-4',
+  'E-3',
+  'C-3',
+  'C-4',
+  'F#3',
+  'D-3',
+  'A-2',
+  'F-3',
+  'E-3',
+  'C-3',
+  'A-2',
+  'C-3',
+  'C-3',
+  'E-3',
+  'C-3',
+  'A-2',
+  'G-2',
+  'A-2',
+  'A-2'
+];
+
+module.exports = _(stairway)
+                  .map(MIDIUtils.noteNameToNoteNumber)
+                  .map(MIDIUtils.noteNumberToFrequency)
+                  .value();
