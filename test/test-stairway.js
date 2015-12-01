ab30512faa3d3d16b103f7693d57b1cba70ad362
Myles Borins <mborins@us.ibm.com>
2015-12-01 02:44:24 -0800

    why did I write this dumb test

---
+++ test/test-stairway.js
@@ -0,0 +1,14 @@
+// 'use strict';
+
+var test = require('tape');
+var _ = require('lodash');
+
+var stairway = require('../src/stairway');
+
+test('stairway:', function (t) {
+  t.plan(28);
+  t.equals(stairway.length, 27, 'There should be 27 notes');
+  _.each(stairway, function (note) {
+    t.equals(typeof note, 'number', 'each note should be a number');
+  });
+});
